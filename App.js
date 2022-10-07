7955f197a9e1e3d684b6f48794c4f13c811d663f
terriadsit <terriadsit@yahoo.com>
2022-10-07 11:45:16 -0500

    fonts added, splash image

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import * as React from 'react';
-
+import { useFonts } from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import BottomTabs from './routes/bottomTabs';
 import MyDrawer from './routes/myDrawer'
 
 export default function App() {
+
+  const logo = require('./assets/splash.png');
+  
+  const [fontsLoaded] = useFonts({
+    'Gothic-Bold': require('./assets/fonts/Oswald-Bold.ttf'),
+    'Gothic': require('./assets/fonts/Oswald-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View>
+        <Image source={logo} style={{ height: 100, width: 100, paddingTop: 100 }}>
+        </Image>
+      </View>
+    )
+  } else
   return (
     <NavigationContainer>
       <MyDrawer />
